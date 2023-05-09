dd0ba703960615fa60fb805559e04d6eee1a8245
tomix421q <zilka.tomas421@gmail.com>
2023-05-09 11:27:58 +0200

    utorok

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
     <>
       {/* Overlay  */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/60  ' />
-      <div className='hero-img flex  items-center  justify-center h-screen bg-fixed bg-center bg-cover'>
-        <div className='absolute max-w-[1200px]   flex flex-col  p-3 lg:top-36 justify-center lg:flex-row'>
+      <div className='hero-img flex items-center  justify-center h-screen bg-fixed bg-center bg-cover'>
+        <div className='absolute max-w-[1200px] flex flex-col  p-3 lg:top-36 justify-center lg:flex-row '>
           {/* BKS H1 */}
-          <div className='flex-col max-w-[750px] font-oswald mb-6'>
-            <h1 className='font-oswald text-7xl  text-gray-200  rounded-md  md:text-[10.625rem] lg:text-[22rem]  '>
+          <div className='flex-col min-w-[290px] font-oswald '>
+            <h1 className='font-oswald text-7xl text-gray-200  rounded-md  md:text-[10.625rem] lg:text-[22rem]'>
               BKS<span className='text-color-green'>.</span>
             </h1>
             {/* Kvalita */}
-            <h2 className=' text-xl text-gray-300 p-2  uppercase  lg:text-3xl lg:ml-4'>
+            <h2 className=' text-xl text-gray-300 p-1  uppercase  lg:text-3xl lg:ml-4'>
               <span className='text-color-green font-bold text-2xl  lg:text-5xl'>
                 100%{' '}
               </span>
@@ -24,7 +24,7 @@ const Hero = () => {
             <TabsHero />
           </div>
           {/* PONUKA */}
-          <div className='font-oswald text-gray-300 p-2 mt-4 w-full lg:mt-10 '>
+          <div className='font-oswald text-gray-300  mt-4 lg:mt-10 min-w-[290px] '>
             <h2 className='text-3xl font-bold uppercase lg:text-5xl'>
               <span className='text-color-green text-3xl lg:text-6xl '>| </span>
               Ponúkame:
@@ -50,7 +50,7 @@ const Hero = () => {
                 Montáž krbov
               </li>
             </ul>
-            <div className='flex justify-around font-robotoLight font-bold  py-4 text-base md:text-2xl '>
+            <div className='flex justify-around font-robotoLight font-bold  py-4 text-sm md:text-2xl '>
               <button class=' bg-color-green hover:bg-color-seablue hover:scale-110 hover:rotate-2 duration-300 transition-all text-gray-900 shadow-2xl py-2 px-4 rounded-md flex uppercase hover:text-gray-200 items-center'>
                 <FaHammer size={25} className='mr-2' />
                 Nasa praca
