f88c5a8e39b0c75b31d5de7d4f20dced08225791
tomix421q <zilka.tomas421@gmail.com>
2023-05-09 23:42:58 +0200

    utorok

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,25 +8,25 @@ const Hero = () => {
     <>
       {/* Overlay  */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-20' />
-      <div className='hero-img flex justify-center h-screen bg-fixed bg-center bg-cover '>
+      <div className='hero-img  flex justify-center h-screen bg-fixed bg-center bg-cover '>
         <div className=' max-w-[1250px] lg:h-[900px] flex flex-col  lg:top-36 lg:flex-row z-30 '>
+          {/* BKS H1 */}
+          <div className='bg-gray-800/50 flex flex-row  min-w-[280px] p-2 font-oswald lg:flex-col '>
+            <h1 className='font-oswald text-7xl text-gray-200  rounded-md  md:text-9xl lg:text-[22rem] plus:text-[6.25rem]'>
+              BKS<span className='text-color-green'>.</span>
+            </h1>
+            {/* Kvalita */}
+            <h2 className=' text-lg text-gray-300 p-2 uppercase plus:text-2xl md:text-4xl  lg:text-3xl lg:ml-4'>
+              <span className='text-color-green font-bold text-2xl md:text-6xl  lg:text-5xl'>
+                100%{' '}
+              </span>
+              kvalita,podľa vaších požiadaviek.
+            </h2>
+            <TabsHero />
+          </div>
           <Smallbanner />
           {/* case without banner */}
           <div className=' max-w-[1250px] lg:h-[900px] p-2 flex flex-col md:p-6 lg:flex-row'>
-            {/* BKS H1 */}
-            <div className=' flex flex-row  min-w-[280px] p-2 font-oswald lg:flex-col '>
-              <h1 className='font-oswald text-7xl text-gray-200  rounded-md  md:text-9xl lg:text-[22rem] plus:text-[6.25rem]'>
-                BKS<span className='text-color-green'>.</span>
-              </h1>
-              {/* Kvalita */}
-              <h2 className=' text-lg text-gray-300 p-2 uppercase plus:text-2xl md:text-4xl  lg:text-3xl lg:ml-4'>
-                <span className='text-color-green font-bold text-2xl md:text-6xl  lg:text-5xl'>
-                  100%{' '}
-                </span>
-                kvalita,podľa vaších požiadaviek.
-              </h2>
-              <TabsHero />
-            </div>
             {/* PONUKA */}
             <div className=' font-oswald text-gray-300   min-w-[280px] max-w-[500px] p-4  lg:mt-10  '>
               <h2 className='text-3xl font-bold uppercase lg:text-6xl lg:mb-10'>
